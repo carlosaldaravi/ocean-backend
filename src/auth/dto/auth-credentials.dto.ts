87d3242b101ef3b41979831096adaabf573cb0e1
Carlos <carlosaldaravi@gmail.com>
2020-03-22 12:02:57 +0100

    added auth and config

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, MinLength, MaxLength, Matches, IsEmail } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsEmail()
+  @MinLength(4)
+  @MaxLength(20)
+  email: string;
+
+//   @IsString()
+//   @MinLength(8)
+//   @MaxLength(20)
+//   @Matches(
+//     /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+//     { message: 'password too weak' },
+//   )
+  password: string;
+}
