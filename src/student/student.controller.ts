afa1d76fc209b8186a5c1a05392a09abb0d6bf45
Carlos <carlosaldaravi@gmail.com>
2020-04-23 14:51:17 +0200

    entities new estructure

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, UsePipes, ValidationPipe, Body, Logger, Get } from '@nestjs/common';
+import { Controller, Post, UsePipes, ValidationPipe, Body, Logger, Get, Patch, Param, ParseIntPipe } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
-import { Student } from './student.entity';
+import { Student } from '../entity/student.entity';
+import { StudentTargets } from 'src/entity/student-target.entity';
+import { GetStudent } from '../decorators/get-student.decorator';
 
 @Controller('student')
 export class StudentController {
@@ -21,4 +23,6 @@ export class StudentController {
         this.logger.verbose(`Creating a new student. Data: ${JSON.stringify(createStudentDto)}`);
         return this.studentService.createStudent(createStudentDto);
     }
+
+
 }
